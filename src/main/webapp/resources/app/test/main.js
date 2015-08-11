9831dbc04c547bc52b1f4e0c575eee27d4c12fc4
NAME <EMAIL>
2015-08-11 13:15:25 -0400

    Sidebar navigation working.

--- src/main/webapp/resources/app/test/main.js
+++ src/main/webapp/resources/app/test/main.js
@@ -8,15 +8,13 @@ import './css/main.css!';
 
 import angular from 'angular';
 import 'angular-ui-router';
-import 'angular-bootstrap';
 
 import { app_route_main } from './route/main/controller';
 
-var ROOT = '/res/app/test';
+var TEMPLATES = '/res/app/test/templates';
 
 export var module = angular.module('app.main', [
   'ui.router',
-  'ui.bootstrap',
   app_route_main.name
 ])
 
@@ -24,14 +22,18 @@ export var module = angular.module('app.main', [
     function($urlRouterProvider,   $stateProvider) {
 
       $urlRouterProvider
-        .otherwise('/main');
+        .otherwise('/main/config');
 
+      // http://odetocode.com/blogs/scott/archive/2014/04/14/deep-linking-a-tabbed-ui-with-angularjs.aspx
       $stateProvider
         .state('main', {
           url: '/main',
-          templateUrl: ROOT + '/route/main/template.html',
+          templateUrl: TEMPLATES + '/route/main.html',
           controller: 'MainViewController'
-        });
+        })
+          .state('main.config',   { url: '/config',   templateUrl: TEMPLATES + '/content/config.html'   })
+          .state('main.profile',  { url: '/profile',  templateUrl: TEMPLATES + '/content/profile.html'  })
+          .state('main.users',    { url: '/users',    templateUrl: TEMPLATES + '/content/users.html'    });
     }
   ])
 
--- src/main/webapp/resources/app/test/route/main/controller.js
+++ src/main/webapp/resources/app/test/route/main/controller.js
@@ -5,16 +5,45 @@
 'use strict';
 
 import angular from 'angular';
+import 'angular-bootstrap';
 
-import { NX } from 'nx/nx';
 import { Test } from '../../util';
 
-export var app_route_main = angular.module('app.route.main', [])
+export var app_route_main = angular.module('app.route.main', [
+  'ui.bootstrap'
+])
 
-  .controller('MainViewController', ['$scope', function($scope) {
-    console.log(`NX: ${NX.version()}`);
-    console.log(NX.queryParams());
+  .controller('MainViewController', ['$rootScope', '$scope', '$state', function($rootScope, $scope, $state) {
 
+    $scope.items = [
+      { title: 'Config',  route: 'main.config',  active: false },
+      { title: 'Profile', route: 'main.profile', active: false },
+      { title: 'Users',   route: 'main.users',   active: false }
+    ];
+
+    $scope.getClass = function(item) {
+      return item.active ? 'active' : '';
+    };
+
+    $scope.getPath = function(item) {
+      return '#' + item.route.replace('\.', '/');
+    };
+
+    $scope.go = function(route) {
+      $state.go(route);
+    };
+
+    $scope.active = function(route) {
+      return $state.is(route);
+    };
+
+    $scope.$on("$stateChangeSuccess", function() {
+      $scope.items.forEach(function(item) {
+        item.active = $scope.active(item.route);
+      });
+    });
+
+    // TODO(burdon): remove.
     var test = new Test();
     test.listen();
     test.fireListeners(100);
