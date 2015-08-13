7d4b75fdc32b27397232a92cee74e2404f0dc6b1
NAME <EMAIL>
2015-08-13 01:13:05 -0400

    Blueprints.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,72 +1,42 @@
 #
 # Copyright 2015 Alien Laboratories, Inc.
 #
-import json
 
 import flask
+import json
 import logging
 from flask.ext.injector import FlaskInjector
-import jinja2
 
-from nx_util.systemjs import SystemJSResolver
+from nx_util.systemjs import systemjs
+from nx_util.docs import docs
 
 LOG = logging.getLogger()
 
 
 class Main(flask.Flask):
 
-    STATIC_PATH = '/res'
-
-    STATIC_FOLDER = '../webapp/resources'
-
     def __init__(self):
         super(Main, self).__init__(
             __name__,
-            static_folder=Main.STATIC_FOLDER,
-            static_url_path=Main.STATIC_PATH,
+            static_folder='../webapp/resources',
+            static_url_path='/res',
             template_folder='templates')
 
         #
         # JSPM Resources.
         #
-        systemjs = SystemJSResolver.create(self, static_folder=Main.STATIC_FOLDER, static_path=Main.STATIC_PATH)
-
-        @self.route('/packages')
-        def packages():
-            return flask.jsonify(systemjs.get_config())
-
-        @self.route('/package:<path:path>')
-        def package(path):
-            return flask.redirect(systemjs.resolve(path))
+        self.register_blueprint(systemjs)
 
         #
         # Docs.
-        # http://flask.pocoo.org/docs/0.10/blueprints/
         #
-
-        from flask import Blueprint, render_template, abort
-        from jinja2 import TemplateNotFound
-
-        # Note: can't easily factor out since folder is relative to package (i.e., __name__)
-        docs = Blueprint('Render Markdown documents.', __name__, template_folder='../webapp/resources/docs')
-
-        @docs.route('/', defaults={'page': 'index'})
-        @docs.route('/<page>')
-        def show(page):
-            try:
-                return render_template('%s.md' % page)
-            except TemplateNotFound:
-                abort(404)
-
-        from flask.ext.markdown import Markdown
-        Markdown(self)
-
         self.register_blueprint(docs, url_prefix='/docs')
 
         #
         # Routes.
         #
         @self.route('/')
+        @self.route('/demo')
         def home():
             return flask.render_template('home.html')
 
